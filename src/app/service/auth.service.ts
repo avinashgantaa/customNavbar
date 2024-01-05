9fdcd9af50b99320093b5d9e9b6d64582ff7efe9
avinashgantaa <avinash.ganta39@gmail.com>
2024-01-05 09:10:12 +0530

    login route

--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -14,7 +14,7 @@ export class AuthService implements CanActivate {
       return true;
     }
     else{
-      this.router.navigate(['/'])
+      this.router.navigate(['/login'])
       return false;
     }
   }
